89f32691532be4b395d0cfe06efb7ed225cd16c6
Kredo-Garry <kredoteacher.grodriguez@gmail.com>
2023-02-10 09:41:36 +0800

    Third upload with edit on index

--- views/index.php
+++ views/index.php
@@ -23,6 +23,7 @@
                         <button type="submit" class="btn btn-primary w-100">Login</button>
                     </form>
                     <p class="text-center mt-3 small"><a href="register.php">Create Account</a></p>
+                    <p class="text-center mt-3 small">The Company App</p>
                 </div>
             </div>
         </div>
